f91fe8d852740833d1679aab5cf43f935dcbfef8
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-04-05 09:08:00 -0400

    d
    d

---
+++ vite-project/src/components/icons/PiniaSupport.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    import { defineStore } from 'pinia'
+
+export const useStore = defineStore('storeId', {
+  // arrow function recommended for full type inference
+  state: () => {
+    return {
+      // all these properties will have their type inferred automatically
+      count: 0,
+      name: 'Eduardo',
+      isAdmin: true,
+      items: [],
+      hasChanged: true,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
