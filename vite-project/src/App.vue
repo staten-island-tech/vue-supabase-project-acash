f4a8aabd654cb877192f068a485658974f0ec065
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-04-12 09:22:48 -0400

    f
    f

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -9,8 +9,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
-      <nav>
+    
+  <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
