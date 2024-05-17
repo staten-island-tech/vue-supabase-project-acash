ba86af00fc989d5a432d569503a4cf5122072664
AkashPak <akashp8@nycstudents.net>
2024-05-17 09:17:40 -0400

    DSA

--- supabase-vue-3/src/router/index.js
+++ supabase-vue-3/src/router/index.js
@@ -2,13 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Auth from '@/components/Auth.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Auth
+      component:  HomeView
+    },
+    {
+      path: '/auth',
+      name: 'Auth',
+      component:  Auth
     },
     {
       path: '/about',
