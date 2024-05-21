fc4ece23c70e484644bbdb5ec31dadf9618fb392
AkashPak <akashp8@nycstudents.net>
2024-05-21 09:24:43 -0400

    Update supabase.js

--- supabase-vue-3/src/supabase.js
+++ supabase-vue-3/src/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-  VITE_SUPABASE_URL= https://gcewydpkzjtqjpknywfm.supabase.co
- VITE_SUPABASE_ANON_KE=Y `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjZXd5ZHBremp0cWpwa255d2ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NDAwMjAsImV4cCI6MjAyODQxNjAyMH0.4QFZVLGC3PyWe3_zGCnWEv55Xo3u_y1k68WD6eh2hQk`
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_KEY
+
+PUBLIC_SUPABASE_URL=https://gcewydpkzjtqjpknywfm.supabase.co
+PUBLIC_SUPABASE_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjZXd5ZHBremp0cWpwa255d2ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NDAwMjAsImV4cCI6MjAyODQxNjAyMH0.4QFZVLGC3PyWe3_zGCnWEv55Xo3u_y1k68WD6eh2hQk'
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
