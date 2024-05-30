ba8f346520c99deb5f0e5c739f1e9002fe70bd52
AkashPak <akashp8@nycstudents.net>
2024-05-30 09:23:45 -0400

    sa

--- supabase-vue-3/src/supabase.js
+++ supabase-vue-3/src/supabase.js
@@ -3,8 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 
 
 
+const supabaseURL= import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseAnonKey= import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
- const supabase = createClient(supabaseUrl, supabaseAnonKey)
+/* const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY */
+ const supabase = createClient(supabaseURL, supabaseAnonKey)
 export{supabase}
